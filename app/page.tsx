8f07cf1ea98921971f24d4f68689da6f71b96fe6
Ida Bagus Dwi Putra Purnawa <44662864+dwiputrapurnawa@users.noreply.github.com>
2025-09-03 11:56:05 +0800

    fix: penambahan use client pada page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import Navbar from "@/components/layout/Navbar";
 import HeroSection from "@/components/sections/HeroSection";
 import AboutSection from "@/components/sections/AboutSection";
